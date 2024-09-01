944d4c460e8824398a7e8294477a9e042858e0ab
richard <theirsaviorhd@gmail.com>
2024-09-01 15:05:57 -0400

    init basic express environment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("hey you!");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+const apiKey = process.env.API_KEY;
+const environment = process.env.NODE_ENV;
+
+console.log(`Server is running on port ${port}`);
+console.log(`API Key: ${apiKey}`);
+console.log(`Environment: ${environment}`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log(__dirname);
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "template.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
