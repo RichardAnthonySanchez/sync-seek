0bac7bbeb827560e8586854c20b0fc6262fc5c77
richard <theirsaviorhd@gmail.com>
2024-09-08 17:01:12 -0400

    the artist input from the user is sent to the corresponding interface as the user sends their input

--- src/controllerArtistsQuery.js
+++ src/controllerArtistsQuery.js
@@ -1,10 +1,12 @@
+import interfaceArtistQuery from "./interfaceArtistQuery";
+
 function artistQuery() {
   document.addEventListener("click", async (event) => {
     let artist;
     if (event.target.id === "submit-form") {
       event.preventDefault();
       artist = document.getElementById("input").value;
-      console.log(artist);
+      interfaceArtistQuery(artist);
     }
     return artist;
   });
