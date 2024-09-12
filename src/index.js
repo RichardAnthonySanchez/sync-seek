21513a218b2561c17b73edc3ebffa7197f918ed8
richard <theirsaviorhd@gmail.com>
2024-09-12 17:50:32 -0400

    call the button to be appended to the main app component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import createForm from "./viewQueryForm";
 import artistQuery from "./controllerArtistsQuery";
+import viewButtonDeleteArtists from "./viewButtonDeleteArtists";
 
 function component() {
   createForm();
   artistQuery();
+  viewButtonDeleteArtists();
 }
 
 document.body.appendChild(component());
