099bc75b027b75aeb42882daf92a0cd1985782ee
richard <theirsaviorhd@gmail.com>
2024-09-15 10:47:36 -0400

    the button now loads to the dom through the entry point of the system

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import createForm from "./viewQueryForm";
 import artistQuery from "./controllerArtistsQuery";
 import viewButtonDeleteArtists from "./viewButtonDeleteArtists";
+import viewButtonSortArtists from "./viewButtonSortArtists";
 
 function component() {
   createForm();
   artistQuery();
   viewButtonDeleteArtists();
+  viewButtonSortArtists();
 }
 
 document.body.appendChild(component());
