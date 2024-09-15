7554d2d987fff727528c323fd07f33e750773d3b
richard <theirsaviorhd@gmail.com>
2024-09-15 06:47:35 -0400

    updated the controller so that deleting multiple stored artist lists is possible

--- src/controllerArtistsQuery.js
+++ src/controllerArtistsQuery.js
@@ -4,14 +4,17 @@ import {
 } from "./interfaceArtistQuery";
 
 function artistQuery() {
+  const listOfKeyNames = [];
   document.addEventListener("click", async (event) => {
     if (event.target.id === "submit-form") {
       event.preventDefault();
       const artistOne = document.getElementById("input1").value;
       const artistTwo = document.getElementById("input2").value;
+      listOfKeyNames.push(artistOne);
+      listOfKeyNames.push(artistTwo);
       interfaceArtistQuery(artistOne, artistTwo);
     } else if (event.target.id === "delete-artists") {
-      deleteStoredArtistsInterface("artistsKey");
+      deleteStoredArtistsInterface(listOfKeyNames);
     }
   });
 }
