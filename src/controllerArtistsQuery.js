968792476f3d3ccb694ac43aa028f868ed742a1c
richard <theirsaviorhd@gmail.com>
2024-09-15 06:12:30 -0400

    changed typos in the controller that were related to the previous way of processing artist data

--- src/controllerArtistsQuery.js
+++ src/controllerArtistsQuery.js
@@ -5,16 +5,14 @@ import {
 
 function artistQuery() {
   document.addEventListener("click", async (event) => {
-    let artist;
     if (event.target.id === "submit-form") {
       event.preventDefault();
-      artistOne = document.getElementById("input1").value;
-      artistTwo = document.getElementById("input2").value;
+      const artistOne = document.getElementById("input1").value;
+      const artistTwo = document.getElementById("input2").value;
       interfaceArtistQuery(artistOne, artistTwo);
     } else if (event.target.id === "delete-artists") {
       deleteStoredArtistsInterface("artistsKey");
     }
-    return artist;
   });
 }
 
