e05f44c6330f9f57e310157ec6b3477495dcadb2
richard <theirsaviorhd@gmail.com>
2024-09-16 17:56:42 -0400

    added a comment to remind myself to make a change to how we recieve user input. with those changes made, the application can handle user input at scale.

--- src/controllerArtistsQuery.js
+++ src/controllerArtistsQuery.js
@@ -9,7 +9,7 @@ function artistQuery() {
   document.addEventListener("click", async (event) => {
     if (event.target.id === "submit-form") {
       event.preventDefault();
-      const artistOne = document.getElementById("input1").value;
+      const artistOne = document.getElementById("input1").value; // a more scalable way to recieve user input is by making each element's selector generic, then using a querySelectorAll method to return an array of user inputs. then we can just create these elements dynmaically.
       const artistTwo = document.getElementById("input2").value;
       listOfKeyNames.push(artistOne);
       listOfKeyNames.push(artistTwo);
