ae4589e176a4a5e9e819107400c29fd6a6960f25
richard <theirsaviorhd@gmail.com>
2024-09-17 18:22:38 -0400

    added a form for a feature where users can query for a similar songs by submitting a song and the correspoinding artist.

---
+++ src/trackGetSimilar/viewTrackGetSimilarForm.js
@@ -0,0 +1,27 @@
+function createTrackGetSimilarForm() {
+  const form = document.createElement("form");
+  form.id = "track-get-similar-form";
+
+  const songNameInput = document.createElement("input");
+  songNameInput.type = "text";
+  songNameInput.class = "song-name-input";
+  songNameInput.placeholder = "Enter song name";
+
+  const artistNameInput = document.createElement("input");
+  artistNameInput.type = "text";
+  artistNameInput.class = "artist-name-input";
+  artistNameInput.placeholder = "Enter artist name";
+
+  const button = document.createElement("button");
+  button.id = "submit-track-get-similar-form";
+  button.type = "submit";
+  button.textContent = "Submit";
+
+  form.appendChild(songNameInput);
+  form.appendChild(artistNameInput);
+  form.appendChild(button);
+
+  document.body.appendChild(form);
+}
+
+export default createTrackGetSimilarForm;
