bcd2943ba74ca745f15daf6775ac8cd97cdd27bc
richard <theirsaviorhd@gmail.com>
2024-09-19 17:46:23 -0400

    updated  the entry point to call the function for the controller so the system can use the input from each added track

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import artistQuery from "./getSimilarArtists/controllerArtistsQuery";
 import viewButtonDeleteArtists from "./deleteArtists/viewButtonDeleteArtists";
 import viewButtonSortArtists from "./sortSimilarArtists/viewButtonSortArtists";
 import createTrackGetSimilarForm from "./trackGetSimilar/viewTrackGetSimilarForm";
+import controllerAddTrack from "./trackGetSimilar/controllerAddTrack";
 
 function component() {
   createTrackGetSimilarForm();
-  artistQuery();
+  //artistQuery(); this is the get similar artists query
+  controllerAddTrack();
   viewButtonDeleteArtists();
   viewButtonSortArtists();
 }
