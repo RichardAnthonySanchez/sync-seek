de4103e0e3abd3c8bc12b3759d05836e10655ff3
richard <theirsaviorhd@gmail.com>
2024-09-21 16:03:57 -0400

    created a method that stores a list of similar tracks to the local storage

---
+++ src/trackGetSimilar/storeSimilarTracks.js
@@ -0,0 +1,12 @@
+export function storeArtistLocally(song, artist, list) {
+  localStorage.setItem(`${song}-${artist}`, JSON.stringify(list));
+  console.log(`storing similar tracks to ${song} by ${artist}...`);
+
+  const similarTracks = localStorage.getItem(`${song}-${artist}`);
+
+  if (similarTracks) {
+    console.log("similar tracks were successfully saved to the local storage");
+  } else {
+    console.error("failed to save similar tracks");
+  }
+}
