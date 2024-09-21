13e79df8830a4baa3cceeae7a615236d10be5eac
richard <theirsaviorhd@gmail.com>
2024-09-21 16:10:12 -0400

    created an interface that recieves similar tracks  using a song name and artist name as well as a corresponding method that stores that list of similar tracks

--- src/trackGetSimilar/interfaceTrackGetSimiliar.js
+++ src/trackGetSimilar/interfaceTrackGetSimiliar.js
@@ -1,8 +1,14 @@
 import fetchTrackGetSimilar from "./fetchTrackGetSimilar";
+import { storeArtistLocally } from "./storeSimilarTracks";
 
-export default function interfaceTrackGetSimilar(artist, song) {
+export async function interfaceTrackGetSimilar(artist, song) {
   console.log(
     "Your artist name is: " + artist + " and your song name is: " + song
   );
-  fetchTrackGetSimilar(artist, song);
+  const similarTracks = await fetchTrackGetSimilar(artist, song);
+  return similarTracks;
+}
+
+export async function storeSimilarTracksList(artist, song, list) {
+  await storeArtistLocally(artist, song, list);
 }
