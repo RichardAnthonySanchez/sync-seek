5021628020eef9aab346aa24f4e14c879472c3bd
richard <theirsaviorhd@gmail.com>
2024-09-21 16:15:00 -0400

    created a fetch request that recieves a song name and artist name then sends that information as a query to the last fm api then  returns a list of similar tracks as a promise

--- src/trackGetSimilar/fetchTrackGetSimilar.js
+++ src/trackGetSimilar/fetchTrackGetSimilar.js
@@ -3,16 +3,14 @@ const apiKey = process.env.API_KEY;
 
 export default async function fetchTrackGetSimilar(artist, song) {
   console.log("fetching for song: " + song + " ...");
-  axios
-    .get(
+  try {
+    const response = await axios.get(
       `https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${artist}&track=${song}&api_key=${apiKey}&format=json`
-    )
-    .then((response) => {
-      const data = response.data;
-      console.log(data);
-      console.log(JSON.stringify(data));
-      // return the data
-      // where do we save the lists?
-    })
-    .catch((error) => console.error(error));
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
