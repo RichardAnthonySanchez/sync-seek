d4aa4a2b0fea4dc2a1c15ad17a7f85d0c36a5ef1
richard <theirsaviorhd@gmail.com>
2024-09-22 17:45:23 -0400

    created a way to store similar track data as well as saving our keys to access that local stoarage data

--- src/trackGetSimilar/storeSimilarTracks.js
+++ src/trackGetSimilar/storeSimilarTracks.js
@@ -1,7 +1,7 @@
 export function storeSimilarTracksLocally(song, artist, list) {
   localStorage.setItem(`${song}-${artist}`, JSON.stringify(list));
   console.log(`storing similar tracks to ${song} by ${artist}...`);
-
+  // get similar tracks to see if they are in the local storage
   const similarTracks = localStorage.getItem(`${song}-${artist}`);
 
   if (similarTracks) {
@@ -9,4 +9,26 @@ export function storeSimilarTracksLocally(song, artist, list) {
   } else {
     console.error("failed to save similar tracks");
   }
+
+  // create a master key of our lists and check each key to see if it is already in the master key list
+  let listOfKeys = JSON.parse(localStorage.getItem("masterKey")) || [];
+
+  if (!listOfKeys.includes(`${song}-${artist}`)) {
+    listOfKeys.push(`${song}-${artist}`);
+    console.log(`successfully pushed, ${song} by ${artist}, to the master key`);
+  } else {
+    console.log("already had that song in the local storage");
+  }
+  // store all non-duplicate the keys to the master key
+  console.log(" your list of master keys are: " + JSON.stringify(listOfKeys));
+  // our keys have whitespace. we will probably want to change that later
+  localStorage.setItem("masterKey", JSON.stringify(listOfKeys));
+}
+
+export function getStoredSimilarTrackLists() {
+  const listOfKeys = JSON.parse(localStorage.getItem("masterKey")) || [];
+
+  const lists = listOfKeys.map((key) => JSON.parse(localStorage.getItem(key)));
+
+  return lists;
 }
