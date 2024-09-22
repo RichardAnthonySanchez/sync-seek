fadb14ecffffdeeeea12e05c1eff4b85e0c419fc
richard <theirsaviorhd@gmail.com>
2024-09-22 06:07:10 -0400

    updated the index to render the add track button by default. this way the user knows they have the option to submit more than one track

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import viewButtonDeleteArtists from "./deleteArtists/viewButtonDeleteArtists";
 import viewButtonSortArtists from "./sortSimilarArtists/viewButtonSortArtists";
-import createTrackGetSimilarForm from "./trackGetSimilar/viewTrackGetSimilarForm";
+import { createTrackGetSimilarForm } from "./trackGetSimilar/viewTrackGetSimilarForm";
 import controllerAddTrack from "./trackGetSimilar/controllerAddTrack";
+import viewAddTrackButton from "./trackGetSimilar/viewAddTrackButton";
 
 function component() {
   createTrackGetSimilarForm();
   //artistQuery(); this is the get similar artists query
   controllerAddTrack();
+  viewAddTrackButton();
   viewButtonDeleteArtists();
   viewButtonSortArtists();
 }
