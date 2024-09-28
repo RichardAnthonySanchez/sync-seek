36b5333076fc605cc009721fb698e3084cec4448
richard <theirsaviorhd@gmail.com>
2024-09-28 07:45:47 -0400

    created a button that will extend our similar songs list

---
+++ src/trackGetSimilar/viewExtendSimilarTracksButton.js
@@ -0,0 +1,6 @@
+export default function ViewExtendSimilarTracksButton() {
+  const button = document.createElement("button");
+  button.id = "extend-similar-tracks";
+  button.innerHTML = "extend list";
+  document.body.appendChild(button);
+}
