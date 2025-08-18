47102c7a263200fd4bc601c9f6bf5d3daf6e1883
richard <theirsaviorhd@gmail.com>
2025-08-18 15:53:09 -0400

    Added methods to dedupe the queue in the prefetch module. this prevents dupes when the use submits the same tracks multiple times

--- src/trackGetSimilar/interfaceFilterPreFetch.js
+++ src/trackGetSimilar/interfaceFilterPreFetch.js
@@ -1,13 +1,28 @@
 export function compareQueueToDB(queue) {
   // normalize strings before comparison
   let normalizedQueue = normalizeQueueData(queue);
-  console.log(normalizedQueue); // can get duplicates after multiple submissions
+  let deduplicatedQueue = deduplicateQueue(normalizedQueue);
+  console.log(deduplicatedQueue);
   // filter based on key (song|title)
 }
 
 //check for matches in the database compared to tracks in the queue
 // if there are matches, create a new queue with only the unique tracks
 // start fetching the unique tracks to the last fm api
+
+function isDuplicate(trackA, trackB) {
+  return JSON.stringify(trackA) === JSON.stringify(trackB);
+}
+
+function deduplicateQueue(queue) {
+  let dedupedQueries = [];
+  //check dupes before pushing
+  if (!dedupedQueries.some((item) => isDuplicate(item, queue))) {
+    // push each query to the queue
+    dedupedQueries.push(queue);
+  }
+  return dedupedQueries;
+}
 
 function normalizeQueueData(queue) {
   let normalizedQueue = queue.map((item) => {
--- src/trackGetSimilar/interfaceTrackQuery.js
+++ src/trackGetSimilar/interfaceTrackQuery.js
@@ -12,6 +12,7 @@ function isDuplicate(trackA, trackB) {
 export function interfaceQueueTrackInQuery(queryObject) {
   //check dupes before pushing
   if (!tracksQuery.some((item) => isDuplicate(item, queryObject))) {
+    // we are doing this deduping conditional in the prefetch later in the pipeline. this can be refactored
     // push each query to the queue
     tracksQuery.push(queryObject);
     // log the total queue arr after each push
