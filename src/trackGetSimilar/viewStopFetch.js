1fa7804883bc7e67c89b6c40c5cdb6cbcc9d5cf7
richard <theirsaviorhd@gmail.com>
2025-08-30 17:42:29 -0400

    removed a file that wasnt being used by the system. removing this duplicate should prevent confusion

--- src/trackGetSimilar/viewStopFetch.js
+++
@@ -1,6 +0,0 @@
-export default function ViewStopFetch() {
-  const button = document.createElement("button");
-  button.id = "view-stop-fetch";
-  button.innerHTML = "stop fetch";
-  document.body.appendChild(button);
-}
